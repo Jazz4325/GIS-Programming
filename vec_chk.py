8511e11f760f900b52e96e5332a7f856026c0305
Jazz4325 <vincent.budak@gmail.com>
2026-01-21 09:33:18 -0800

    Added info option
    Can now use -i or --info optional argument that displays metadata about shapefile (feature count, CRS, bounds, any missing geometries)

--- vec_chk.py
+++ vec_chk.py
@@ -4,12 +4,16 @@ from pandasgui import show
 import argparse
 
 parser = argparse.ArgumentParser()
-actions = parser.add_mutually_exclusive_group()
+parser.add_argument("input", help="Input shapefile ZIP file")
+
+actions = parser.add_mutually_exclusive_group(required=True)
+actions.add_argument("-i", "--info", action="store_true", help="Print basic information about the vector dataset.")
 actions.add_argument("-p", "--plot", action="store_true", help="Plot your shapefile using matplotlib.")
 actions.add_argument("-b", "--browse", action="store_true",help="Browse your shapefile's attribute table.")
 actions.add_argument("-e", "--export", action="store_true",help="Export your shapefile's attributes as a .csv file.")
-parser.add_argument("input", help="Input shapefile ZIP file")
+
 args = parser.parse_args()
+
 
 
 # 1. Load Shapefile ZIP as GeoDataFrame [geopandas]
@@ -17,20 +21,43 @@ args = parser.parse_args()
 gdf = gpd.read_file(args.input)  
 
 
-# 2. Quick plot [matplotlib]
+# 2. Display metadata about shapefile
+
+
+if args.info:
+    print("\n--- Vector dataset info ---")
+    print(f"File: {args.input}")
+    print(f"Feature count: {len(gdf)}")
+    print(" ------------------CRS------------------------")
+    crs = gdf.crs
+    if crs:
+        print("CRS code:", crs.to_string())
+        print("CRS name:", crs.name)
+    else:
+        print("CRS: None (no CRS defined)")
+    print(" ------------------Bounds------------------------")
+    print("    " + "|min-x| \t |min-y| \t |max-x| \t |max-y|")
+    print(gdf.total_bounds) 
+    print("-------------------------------------------------")
+    print("  Missing geometries:", gdf.geometry.isna().sum())
+    print("  Invalid geometries:", (~gdf.is_valid).sum())
+
+
+# 3. Quick plot [matplotlib]
 
 if args.plot:
     gdf.plot()
     plt.show()
 
 
-# 3. Browse Attribute Table - (THIS IS GOATED) [pandasgui]
+# 4. Browse Attribute Table - (THIS IS GOATED) [pandasgui]
 
 if args.browse:
     show(gdf) 
+ 
 
 
-# 4. Save as .csv (default saves to current working directory) [geopandas]
+# 5. Save as .csv (default saves to current working directory) [geopandas]
 # -- save only specific fields --
     # fields = ["BLOCKID", "POP100", "HU100", "geometry"]
     # gdf = gdf[fields] 
